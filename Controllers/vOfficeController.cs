984c2852c6209bccb9bd2675d7f9f150188fb03c
lissettecidm@gmail.com <lissettecidm@gmail.com>
2021-01-02 17:36:17 -0400

    Primer intento de la oficina virtual (Cuando el usuario está loggeado) (Creación de vOfficeController/Index)

---
+++ Controllers/vOfficeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetBanking.Controllers
+{
+    public class vOfficeController : Controller
+    {
+        // GET: vOffice
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
